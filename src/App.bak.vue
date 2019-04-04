2cbdbee2e11b6b8bf391c892979900ddaa09761a
AmaliaRVM <amaliarvm@gmail.com>
2019-04-04 16:55:13 +0200

    ddailies

---
+++ src/App.bak.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+      <weatherAPI/>
+      Hello
+        <!-- <img src="assets/images/sunny.svg"> {{overcast}}
+        <span class="temperature">{{currentTemp}}°</span><br>
+        <span id="temp-values">Min {{minTemp}}° <br> Max {{maxTemp}}°</span>
+  
+      <div id="info">
+        <img class="icon" :src=icon> {{sunrise}}
+        <img class="icon" src="assets/images/sunset.svg"> {{sunset}}
+        <img class="icon" src="assets/images/humidity.svg"> {{humidity}}
+        <img class="icon" src="assets/images/pressure.svg"> {{pressure}}
+        <img class="icon" src="assets/images/wind.svg"> {{wind}}
+      </div> -->
+  </div>
+</template>
+
+<script>
+import weatherAPI from './components/weatherAPI.vue'
+
+
+export default {
+  name: 'app',
+  components: {
+    weatherAPI
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smimport './plugins/bootstrap-vue'
+sed;
+  -moz-osx-font-simport './plugins/bootstrap-vue'
+le;
+  text-align: cenimport './plugins/bootstrap-vue'
+
+  color: #2c3e50;import './plugins/bootstrap-vue'
+
+  margin-top: 60px;
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>O shit waddup</h1>
+    <form @submit.prevent="getWeather()">
+      <div class="row">
+        <div class="offset-md-3 col-md-6">
+          <div class="form-group">
+            <input v-model="inputdata" type="text" class="form-control" aria-describedby="browsecity" placeholder="Enter city">
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="offset-md-2 col-md-8">
+          <div class="row">
+            <div class="col-md-12 border border-dark">
+              <HelloWorld/>
+            </div>
+          </div>
+          <div class="row">
+            <div class="offset-md-1 col-md-2 border border-dark"><p></p></div>
+            <div class="col-md-2 border border-dark"><p>{{inputdata}}</p></div>
+            <div class="col-md-2 border border-dark"><p>{{currentTemp}}</p></div>
+            <div class="col-md-2 border border-dark"><p>{{icon}}</p></div>
+            <div class="col-md-2 border border-dark"><img src="assets/img/01.svg" alt="icon"></div>
+          </div>
+        </div>
+      </div>
+    </form>
   </div>
-  <input type="text" placeholder="" id="search" />
-    <button id="searchbutton">Yolo</button>
-    <div id="target"></div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import weatherAPI from '.components/weatherAPI'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      resp: null,
+      inputdata: "",
+      currentTemp: "",
+      minTemp:"",
+      maxTemp:"",
+      sunrise:"",
+      sunset:"",
+      pressure:"",
+      humidity:"",
+      wind:"",
+      overcast:"",
+      icon:""
+    }
+  },
+  methods:{
+    getWeather(){
+      let url=`http://api.openweathermap.org/data/2.5/weather?q=${this.inputdata}&units=metric&APPID=fead09aadf4f208af0a79233ca98019d`;
+      axios
+        .get(url)
+        .then(response => {
+          this.resp = response.data;
+          this.currentTemp = this.resp.main.temp;
+          this.minTemp = this.resp.main.temp_min;
+          this.maxTemp = this.resp.main.temp_max;
+          this.pressure = this.resp.main.pressure;
+          this.humidity = this.resp.main.humidity + "%";
+          this.wind = this.resp.wind.speed + "m/s";
+          this.overcast = this.resp.weather[0].description;
+          this.icon = "/assets/img/" + this.resp.weather[0].icon.slice(0,2) + ".png";
+          this.sunrise = new Date(this.resp.sys.sunrise*1000).toLocaleTimeString("fr-FR").slice(0.4);
+          this.sunset = new Date(this.resp.sys.sunset*1000).toLocaleTimeString("fr-FR").slice(0.4);
+        })
+      .catch(error => {
+        throw(error);
+      });
+    },
+    beforeMonth(){
+      this.getWeather();
+    }
   }
 }
 </script>
